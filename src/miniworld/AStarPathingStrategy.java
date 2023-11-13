203d676a0031ab862c063f3470418f7370809688
noahgibony <noahgiboney@gmail.com>
2023-11-13 11:54:41 -0800

    added miniworld for testing A* pathing

---
+++ src/miniworld/AStarPathingStrategy.java
@@ -0,0 +1,34 @@
+package miniworld;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.LinkedList;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.HashMap;
+
+class AStarPathingStrategy implements PathingStrategy {
+
+
+    public List<Point> computePath(Point start, Point end,
+                                   Predicate<Point> canPassThrough,
+                                   BiPredicate<Point, Point> withinReach,
+                                   Function<Point, Stream<Point>> potentialNeighbors)
+    {
+        List<Point> path = new LinkedList<>();
+        /*define closed list
+          define open list
+          while (true){
+            Filtered list containing neighbors you can actually move to
+            Check if any of the neighbors are beside the target
+            set the g, h, f values
+            add them to open list if not in open list
+            add the selected node to close list
+          return path*/  
+         return path;
+    }
+}
