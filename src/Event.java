a1a72cd400f042f906f7f5cae4ba7517739842e4
noahgiboney <noahgiboney@gmail.com>
2023-12-12 19:34:08 -0800

    class to record

--- src/Event.java
+++ src/Event.java
@@ -2,17 +2,5 @@
  * An event is made up of an Entity that is taking an
  * Action a specified time.
  */
-public final class Event {
-    private final Action action;
-    private final double time;
-    private final Entity entity;
-
-    public Event(Action action, double time, Entity entity) {
-        this.action = action;
-        this.time = time;
-        this.entity = entity;
-    }
-    public Action action(){return this.action;}
-    public double time(){return this.time;}
-    public Entity entity(){return this.entity;}
+public record Event(Action action, double time, Entity entity) {
 }
